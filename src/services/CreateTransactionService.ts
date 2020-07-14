bf35feb2d36fb7e6f0629beb3e66e95cf15ec8d9
Natan Souza <42328139+natan1506@users.noreply.github.com>
2020-07-14 09:02:02 -0300

    first commit -m

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,10 +1,59 @@
 // import AppError from '../errors/AppError';
+import { getCustomRepository, getRepository } from 'typeorm';
+import AppError from '../errors/AppError';
+import TransactionsRepository from '../repositories/TransactionsRepository';
 
 import Transaction from '../models/Transaction';
+import Category from '../models/Category';
 
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+  category: string;
+}
 class CreateTransactionService {
-  public async execute(): Promise<Transaction> {
-    // TODO
+  public async execute({
+    title,
+    value,
+    type,
+    category,
+  }: Request): Promise<Transaction> {
+    const transactionsRepository = getCustomRepository(TransactionsRepository);
+    const categoryRepository = getRepository(Category);
+
+    let categoryExist = await categoryRepository.findOne({
+      where: { title: category },
+    });
+
+    if (!categoryExist) {
+      categoryExist = categoryRepository.create({
+        title: category,
+      });
+
+      await categoryRepository.save(categoryExist);
+    }
+
+    if (type === 'outcome') {
+      const { total } = await transactionsRepository.getBalance();
+
+      if (total - value < 0) {
+        throw new AppError(
+          "You can't create an outcome that are greater than your total",
+        );
+      }
+    }
+
+    const transaction = transactionsRepository.create({
+      title,
+      value,
+      category: categoryExist,
+      type,
+    });
+
+    await transactionsRepository.save(transaction);
+
+    return transaction;
   }
 }
 
